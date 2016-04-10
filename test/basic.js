85aaabecf732484f78696a99fdbfd5ffb7bf3919
Ivshti <ivo@strem.io>
2016-04-11 01:55:39 +0300

    added more test variations

--- test/basic.js
+++ test/basic.js
@@ -1,11 +1,46 @@
 var tape = require("tape");
 var parser = require("../index");
 
-tape("parses show, classic stamp", function(t) {
-    var x = parser("pioneer.one.s01e05.hdtv.mp4");
-    t.equals(x.type, "series");
-    t.equals(x.name, "pioneer one");
-    t.equals(x.season, 1);
-    t.equals(x.episode[0], 5);
-    t.end();
-})
+var shows = [
+    "pioneer.one.s01e01.avi",       //show with long-md
+    //"pioneer.one.s1e1.avi",         //show with short-md //never seen
+    "pioneer.one.1x1.avi",          //show with alt-short-md
+    "pioneer.one.01x01.avi",        //show with alt-long-md
+    "pioneer.one.season.1.episode.1.avi", //show with full md //never seen
+    "pioneer.one.1001.avi",         //show with unmarked md
+    "pioneer.one.2014.217.avi",     //show with year & unmarked short-md
+    "pioneer.one.2014.1017.avi",    //show with year & unmarked long-md
+    "pioneer.one.2014.s01e01.avi",  //show with year & long-md
+    "pioneer.one.101.s01e01.avi",        //show with stamp & long-md
+    "pioneer.one.101.2014.s01e01.avi"   //total fuck up
+];
+
+shows.forEach(function(str) {
+	tape("show test - "+str, function(t) {
+		var x = parser(str);
+		t.equals(x.type, "series");
+		t.equals(x.name, "pioneer one");
+		if (x.year) t.equals(x.year, 2014);
+		t.equals(x.season, 1);
+		t.equals(x.episode && x.episode[0], 1);
+		t.end();
+	})
+});
+
+var movies = [
+ // "pioneer.one.avi",        //movie
+  "pioneer.one.2014.avi",   //movie with year
+  "pioneer.one.201.2014.avi",   //movie with stamp & year
+  //"pioneer.one.1980.2014.avi",  //movie with longstamp & year (new movie)
+  //"pioneer.one.2020.2014.avi"   //movie with longstam & year (old movie)
+];
+
+movies.forEach(function(str) {
+	tape("movie test - "+str, function(t) {
+		var x = parser(str);
+		t.equals(x.type, "movie");
+		t.equals(x.name, "pioneer one");
+		t.equals(x.year, 2014);
+		t.end();
+	})
+});
